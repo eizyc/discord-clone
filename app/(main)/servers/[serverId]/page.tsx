d2afb08245069ed54e2562ea81af620c24ff10b8
eizyc <yizhichen98@gmail.com>
2024-11-17 00:58:42 -0600

    feat(channel): channel id page

--- app/(main)/servers/[serverId]/page.tsx
+++ app/(main)/servers/[serverId]/page.tsx
@@ -1,7 +1,45 @@
-const ServerIdPage = () => {
-    return ( 
-      <div>Server ID Page</div>
-     );
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
   }
-   
-  export default ServerIdPage;
+};
+const ServerIdPage = async ({
+  params
+}: ServerIdPageProps) => {
+  const { redirectToSignIn } = await auth();
+  const profile = await currentProfile();
+  if (!profile) {
+    return redirectToSignIn();
+  }
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        }
+      }
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general"
+        },
+        orderBy: {
+          createdAt: "asc"
+        }
+      }
+    }
+  })
+  const initialChannel = server?.channels[0];
+  if (initialChannel?.name !== "general") {
+    return null;
+  }
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`)
+}
+
+export default ServerIdPage;
