d405ea0252cbd57298cb4f9d02ca458ad07a17d7
eizyc <yizhichen98@gmail.com>
2024-11-16 23:43:45 -0600

    feat(server):  delete and leave server modal

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -2,6 +2,30 @@
 import { NextResponse } from "next/server";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
+
+export async function DELETE(
+    req: Request,
+    { params }: { params: { serverId: string } }
+  ) {
+    try {
+    const { serverId } = await params
+      const profile = await currentProfile();
+      if (!profile) {
+        return new NextResponse("Unauthorized", { status: 401 });
+      }
+      const server = await db.server.delete({
+        where: {
+          id: serverId,
+          profileId: profile.id,
+        }
+      });
+      return NextResponse.json(server);
+    } catch (error) {
+      console.log("[SERVER_ID_DELETE]", error);
+      return new NextResponse("Internal Error", { status: 500 });
+    }
+  }
+
 export async function PATCH(
   req: Request,
   { params }: { params: { serverId: string } }
--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -5,6 +5,8 @@ import { InviteModal } from "@/components/modals/invite-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { MembersModal } from "@/components/modals/members-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
+import { LeaveServerModal } from "@/components/modals/leave-server-modal";
+import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 
 import { useClient } from "@/hooks/use-client";
 
@@ -19,6 +21,8 @@ export const ModalProvider = () => {
         <EditServerModal />
         <MembersModal />
         <CreateChannelModal />
+        <LeaveServerModal />
+        <DeleteServerModal />
       </>
     )
   );
